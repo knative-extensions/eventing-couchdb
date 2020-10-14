3ec0f38e585bdb217a26be6f826aeb99b41dfc12
Scott Nichols <snichols@vmware.com>
2020-10-14 12:50:34 -0700

    leverage new test-infra go_update_deps

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -13,10 +13,53 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 set -o errexit
 set -o nounset
 set -o pipefail
 
-source $(dirname "$0")/../vendor/knative.dev/test-infra/scripts/library.sh
+export GO111MODULE=on
 
-go_update_deps "$@"
+source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/library.sh
+
+cd ${REPO_ROOT_DIR}
+
+VERSION="master"
+
+# The list of dependencies that we track at HEAD and periodically
+# float forward in this repository.
+FLOATING_DEPS=(
+  "knative.dev/pkg@release-0.18"
+  "knative.dev/eventing@${VERSION}"
+  "knative.dev/test-infra@release-0.18"
+)
+
+# Parse flags to determine any we should pass to dep.
+GO_GET=0
+while [[ $# -ne 0 ]]; do
+  parameter=$1
+  case ${parameter} in
+    --upgrade) GO_GET=1 ;;
+    *) abort "unknown option ${parameter}" ;;
+  esac
+  shift
+done
+readonly GO_GET
+
+if (( GO_GET )); then
+  go get -d ${FLOATING_DEPS[@]}
+fi
+
+# Prune modules.
+go mod tidy
+go mod vendor
+
+rm -rf $(find vendor/ -name 'OWNERS')
+rm -rf $(find vendor/ -name 'OWNERS_ALIASES')
+rm -rf $(find vendor/ -name 'BUILD')
+rm -rf $(find vendor/ -name 'BUILD.bazel')
+rm -rf $(find vendor/ -name '*_test.go')
+
+export GOFLAGS=-mod=vendor
+
+update_licenses third_party/VENDOR-LICENSE "./..."
